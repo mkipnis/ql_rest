027d23ceee82facf2d1db92315f7eb2f0d146348
Mike Kipnis <mike.kipnis@gmail.com>
2023-04-15 12:21:55 -0400

    updated

--- docker/build_ql_rest.sh
+++ docker/build_ql_rest.sh
@@ -2,7 +2,7 @@
 
 git clone https://github.com/mkipnis/ql_rest ql_rest
 cd ql_rest
-git checkout docker
+#git checkout docker
 
 ./configure --with-deps-include=/usr/local/include/ --with-deps-lib=/usr/local/lib
 make install
