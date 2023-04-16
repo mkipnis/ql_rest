b8e712e4ccafa407588c853593505a35573ad393
Mike Kipnis <mike.kipnis@gmail.com>
2023-04-16 09:49:24 -0400

    Docker image size optimization

--- docker/dockerize_it.sh
+++ docker/dockerize_it.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+#docker build -t ghcr.io/mkipnis/ql_rest:latest  -f docker_build_all .
+
 docker build -t ql_rest -f docker_build_all .
+docker push ghcr.io/mkipnis/ql_rest:latest
+
 
 # docker run --rm -ti -v "$PWD:/opt/workspace" ql_rest
