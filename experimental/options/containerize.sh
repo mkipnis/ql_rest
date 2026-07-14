0bef10e6a22b4d99ce5d6d7f6aa57d96592eb599
Mike Kipnis <mike.kipnis@gmail.com>
2026-07-14 22:02:02 +0000

    docker update

--- experimental/options/containerize.sh
+++ experimental/options/containerize.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-docker build -t options_pricer_cpp . -f Dockerfile.server_cpp
-docker build -t options_pricer_py . -f Dockerfile.server_py
+docker build -t ghcr.io/mkipnis/ql_rest_opt_pricer_cpp:latest . -f Dockerfile.server_cpp
+docker build -t ghcr.io/mkipnis/ql_rest_opt_pricer_py:latest . -f Dockerfile.server_py
+docker build -t ghcr.io/mkipnis/ql_rest_opt_pricer_client:latest . -f Dockerfile.client
 
-docker build -t options_pricer_client . -f Dockerfile.client
+docker push ghcr.io/mkipnis/ql_rest_opt_pricer_cpp:latest
+docker push ghcr.io/mkipnis/ql_rest_opt_pricer_py:latest
+docker push ghcr.io/mkipnis/ql_rest_opt_pricer_client:latest
