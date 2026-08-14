8c15d80d558251b6e26537121d1970bd0ec6f597
Mike Kipnis <mike.kipnis@gmail.com>
2026-08-14 13:48:38 +0000

    uvicorn scaling

--- experimental/options/containerize.sh
+++ experimental/options/containerize.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-docker build -t ghcr.io/mkipnis/ql_rest_opt_pricer_cpp:latest . -f Dockerfile.server_cpp
-docker build -t ghcr.io/mkipnis/ql_rest_opt_pricer_py:latest . -f Dockerfile.server_py
-docker build -t ghcr.io/mkipnis/ql_rest_opt_pricer_client:latest . -f Dockerfile.client
+docker build --no-cache -t ghcr.io/mkipnis/ql_rest_opt_pricer_cpp:latest . -f Dockerfile.server_cpp
+docker build --no-cache -t ghcr.io/mkipnis/ql_rest_opt_pricer_py:latest . -f Dockerfile.server_py
+docker build --no-cache -t ghcr.io/mkipnis/ql_rest_opt_pricer_client:latest . -f Dockerfile.client
 
 docker push ghcr.io/mkipnis/ql_rest_opt_pricer_cpp:latest
 docker push ghcr.io/mkipnis/ql_rest_opt_pricer_py:latest
